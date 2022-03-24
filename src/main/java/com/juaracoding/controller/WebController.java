b7c0b2fc66ba74e8848ed9c9eb713700f76a0342
Nip <roughchaser@gmail.com>
2022-03-24 15:52:19 +0700

    add image upload

--- src/main/java/com/juaracoding/controller/WebController.java
+++ src/main/java/com/juaracoding/controller/WebController.java
@@ -1,17 +1,20 @@
 package com.juaracoding.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.juaracoding.model.ContentModel;
 import com.juaracoding.model.JudulModel;
@@ -19,6 +22,7 @@ import com.juaracoding.model.TestiModel;
 import com.juaracoding.model.UserModel;
 import com.juaracoding.repository.ComfortRepository;
 import com.juaracoding.repository.UserRepository;
+import com.juaracoding.utility.FileUtility;
 
 @Controller
 public class WebController {
@@ -113,7 +117,15 @@ public class WebController {
 	
 	
 	@PostMapping("/blog/input")
-	private String saveBlog(@ModelAttribute UserModel data) {
+	private String saveBlog(@ModelAttribute UserModel data,
+			@RequestParam(value="file")MultipartFile file) throws IOException {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		String uploadDir = "user-images";
+		
+//		cara membaca image yang pertama data.setGambar("/"+uploadDir+"/"+fileName);
+
+		data.setGambar(fileName);
+		FileUtility.simpanFile(uploadDir, fileName, file);
 		userRepository.save(data);
 		return "redirect:/blog";
 	}
